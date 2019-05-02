568da42083928e1466f7d14163e1ebd5da1f964c
Cesar Augusto <cesarbruschetta@hotmail.com>
2019-05-02 23:34:30 +0000

    Add version 2

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import re
 
-REGEX = r"^([a-z0-9-._]+)@([a-z0-9]+)(\.[a-z]{1,3})+$"
-
-
-def valid_email(email):
-    match = re.match(REGEX, email)
-    if match:
-        return True
-    return False
+REGEX = r"^([a-z0-9-._]+)@([a-z0-9]+)(\.[a-z]{1,3}){1,2}$"
 
 
 def filter_email(emails):
-    return [email for email in emails if valid_email(email)]
+    return list(filter(lambda e: re.match(REGEX, e), emails))
